452a2159682f0fb0f8301367b00b417a238b37e6
HamidNori <hano07001@utb.vaxjo.se>
2025-01-15 12:31:41 +0100

    saker

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+class Program 
+{
+    static void Main()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        int[] a = Enumerable.Range(1, 100).OrderBy(x => Guid.NewGuid()).ToArray();
+
+        BubbleSort(a);
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.Write($"{a[i]}");
+            
+        }
+        stopwatch.Stop();
+        Console.WriteLine($"Time taken: {stopwatch.ElapsedMilliseconds} ms");
+    }
+    private static void BubbleSort(int[] a)
+    {
+        int cnt = a.Length;
+        bool sorted = false;
+        while(!sorted)
+        {
+            sorted = true;
+            for(int i = 0; i < cnt - 1; i++)
+            {
+                if(a[i] > a[i+1])
+                {
+                    int temp = a[i];
+                    a[i] = a[i+1];
+                    a[i+1] = temp;
+                    sorted = false;
+                }
+            }
+            cnt-= 1;
+            
+        }
+        
+    }
+    
+}
